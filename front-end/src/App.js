6e80b28b0baed7e72300245c24fa948b5cba7a53
EstebanCardoso44 <90623113+EstebanCardoso44@users.noreply.github.com>
2024-01-16 15:13:45 +0100

    liasion front-api

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter,Route,Routes,} from "react-router-dom";
+import { BrowserRouter, Route, Routes, } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
@@ -7,18 +7,18 @@ import Recette from "./pages/recette";
 import './App.css'
 
 function App() {
-  return (
-      <BrowserRouter>
-          <div className="navbar">
-              <Navbar/>
-          </div>
-              <Routes>
-                  <Route exact path="/"  element={<Home></Home>}/>
-                  <Route path="Contact" element={<Contact></Contact>}/>
-                  <Route path="Recette" element={<Recette></Recette>}/>
-              </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="navbar">
+                <Navbar />
+            </div>
+            <Routes>
+                <Route exact path="/" element={<Home></Home>} />
+                <Route path="Contact" element={<Contact></Contact>} />
+                <Route path="Recette" element={<Recette></Recette>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- front-end/src/pages/recette.js
+++ front-end/src/pages/recette.js
@@ -1,13 +1,48 @@
-import React from 'react';
-import '../App.css'
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+
+const backendUrl = 'http://localhost:2000';
+
 const Recette = () => {
+  const [recetteData, setRecetteData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${backendUrl}/recette`);
+
+        console.log(response);
+
+        if (!response.ok) {
+          throw new Error(`Erreur lors de la requête : ${response.status}`);
+        }
+
+        const data = await response.json();
+        setRecetteData(data);
+      } catch (error) {
+        console.error(`Une erreur s'est produite : ${error.message}`);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="contact">
-      <h2>Contactez-nous</h2>
-      <p>
-        Vous pouvez nous contacter à l'adresse email suivante :{' '}
-        <a href="mailto:contact@example.com">contact@example.com</a>
-      </p>
+    <div>
+      <h1>Recette</h1>
+      {recetteData ? (
+        <div>
+          {recetteData.map((recipe) => (
+            <div key={recipe.id}>
+              <p>ID: {recipe.id}</p>
+              <p>Nom de la recette: {recipe.recipe_name}</p>
+              {/* Ajoutez d'autres éléments en fonction de votre structure de données */}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Chargement en cours...</p>
+      )}
     </div>
   );
 };
